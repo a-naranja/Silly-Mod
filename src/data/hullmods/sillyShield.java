6a990ea7f7b4f6d5c7da4ea5b8a19de72891655e
a-naranja <albertgoga2@gmail.com>
2026-03-09 14:09:11 +0100

    fixing videolib stuff improved sillyengine improved silly shield fx improved arrow missile system stuff

--- src/data/hullmods/sillyShield.java
+++ src/data/hullmods/sillyShield.java
@@ -7,7 +7,7 @@ public class sillyShield extends BaseHullMod {
     public String sillyShield = "sillyShield";
     public void applyEffectsAfterShipCreation(ShipAPI ship, String id){
         if(!ship.getCustomData().containsKey("appliedShield")){
-            ship.getShield().setRadius(ship.getShield().getRadius(), "graphics/fx/sillyShield2.png", "graphics/fx/sillyShield2.png");
+            ship.getShield().setRadius(ship.getShield().getRadius(), "graphics/fx/sillyShieldFX.png", "graphics/fx/sillyShieldFX.png");
             ship.setCustomData("appliedShield",1);
         }
     }
